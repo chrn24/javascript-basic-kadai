16747ff4905211c04af4473225e83f662c68fd32
chrn24 <chrn-24@outlook.jp>
2025-03-25 21:59:44 +0900

    03252159

---
+++ kadai_021/event.js
@@ -0,0 +1,10 @@
+const btn=document.getElementById('btn');
+const text=document.getElementById('text');
+
+btn,addEventListener('click',()=>{
+    //クリックされたら2秒後にメッセージを変更
+    setTimeout(()=>{
+        text.textContent='ボタンをクリックしました';
+    },2000);
+ });
+
