b1cea7b617d203747d4e4daa34814e2f72e0974a
chrn24 <chrn-24@outlook.jp>
2025-03-25 22:03:45 +0900

    03252203

--- kadai_021/event.js
+++ kadai_021/event.js
@@ -1,7 +1,7 @@
 const btn=document.getElementById('btn');
 const text=document.getElementById('text');
 
-btn,addEventListener('click',()=>{
+btn.addEventListener('click',()=>{
     //クリックされたら2秒後にメッセージを変更
     setTimeout(()=>{
         text.textContent='ボタンをクリックしました';
